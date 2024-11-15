0640c4c6d170c302ea073661bbb14a7836f078bc
kiku69 <merekristofer@gmail.com>
2024-11-15 09:51:15 +0200

    commit

---
+++ src/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex-grow text-center text-orange-200">
+    <h1 class="text-4xl">About Page</h1>
+    <p>Welcome to the About Page!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+/* Styles specific to About.vue */
+</style>
+
